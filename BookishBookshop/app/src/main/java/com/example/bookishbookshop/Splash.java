3cd309acc3be9cb4a2d743619942f296f571b75a
Helen Bernal <helenlbv17@gmail.com>
2021-07-14 17:39:48 -0500

    splash 2.0

--- BookishBookshop/app/src/main/java/com/example/bookishbookshop/Splash.java
+++ BookishBookshop/app/src/main/java/com/example/bookishbookshop/Splash.java
@@ -1,10 +1,13 @@
 package com.example.bookishbookshop;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.VideoView;
 
 public class Splash extends AppCompatActivity {
@@ -14,24 +17,25 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
         getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+        videoView = findViewById(R.id.videoView);
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.bookish);
+        videoView.setVideoURI(video);
+        videoView.setZOrderOnTop(true);
+        videoView.start();
+
 
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
                 Intent intent = new Intent(Splash.this, Login.class);
                 startActivity(intent);
-
-                videoView = findViewById(R.id.videoView);
-
-                String path = "android.resource://"+getPackageName()+"/"+R.raw.bookish;
-
-                videoView.setVideoURI(Uri.parse(path));
-                videoView.start();
+                finish();
             }
 
-        },4000);
+        },5500);
+
+
     }
 }
