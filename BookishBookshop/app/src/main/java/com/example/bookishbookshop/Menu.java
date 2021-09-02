9e9c8194f4aaf0d8db5ae6496645c18da08f7271
Helen Bernal <helenlbv17@gmail.com>
2021-09-01 23:10:49 -0500

    textview rojo

--- BookishBookshop/app/src/main/java/com/example/bookishbookshop/Menu.java
+++ BookishBookshop/app/src/main/java/com/example/bookishbookshop/Menu.java
@@ -1,8 +1,8 @@
 package com.example.bookishbookshop;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,5 +22,21 @@ public class Menu extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+
+            /*private Button button;
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                button= (Button)findViewById(R.id.button);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this, UnityPlayerActivity.class);
+                        startActivity(i);
+                    }
+                });
+            }*/
     }
 }
