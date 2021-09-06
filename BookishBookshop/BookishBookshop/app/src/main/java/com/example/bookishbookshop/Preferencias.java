9f7ded5b323b6ab71d4e35a988c42df82a1ca8d2
Helen Bernal <helenlbv17@gmail.com>
2021-09-06 14:05:47 -0500

    update preferencias autofit

--- BookishBookshop/BookishBookshop/app/src/main/java/com/example/bookishbookshop/Preferencias.java
+++ BookishBookshop/BookishBookshop/app/src/main/java/com/example/bookishbookshop/Preferencias.java
@@ -4,12 +4,25 @@ import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class Preferencias extends AppCompatActivity {
 
+    /*
+     * ---Variables--------------------------------------------------------------------------------*/
+    ImageView logo;
+    ScrollView scrvP1;
     ConstraintLayout botonOmitir,botonSiguiente;
+
+    /*
+     * ---AutoFit Variables-----------------------------------------------------------------------*/
+    private int dpWidth, dpHeight, designWidth = 360,  desighHeiht = 780;
+    private float dDensity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,10 @@ public class Preferencias extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_preferencias);
 
+        /*
+         * ---Asignar Variables-------------------------------------------------------------------*/
+        logo = findViewById(R.id.iv_Logo_Preferences1);
+        scrvP1 = findViewById(R.id.scrollView_Preferences1);
         botonOmitir = findViewById(R.id.btnOmitir);
         botonSiguiente = findViewById(R.id.btnSiguiente);
 
@@ -35,6 +52,40 @@ public class Preferencias extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+
+        /*
+         * ---AutoFit-----------------------------------------------------------------------------*/
+        int buttonsX = 130, buttonsY = 60, edittextX = 300, edittextY = 70;
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        dpWidth = (displayMetrics.widthPixels);
+        dpHeight = (displayMetrics.heightPixels);
+        dDensity = (displayMetrics.scaledDensity);
+        // Imageview: Logo
+        ViewGroup.LayoutParams logoP = (ViewGroup.MarginLayoutParams) logo.getLayoutParams();
+        logoP.height = calHeight(120);
+        // ScrollView: scrollView_Preferences1
+        ViewGroup.LayoutParams scrvP1P = (ViewGroup.MarginLayoutParams) scrvP1.getLayoutParams();
+        scrvP1P.height = calHeight(608);
+        // ConstraintLayout: Cancelar
+        ViewGroup.LayoutParams btnOmitirP = (ViewGroup.MarginLayoutParams) botonOmitir.getLayoutParams();
+        btnOmitirP.width = calWidth(buttonsX);
+        btnOmitirP.height = calHeight(buttonsY);
+        // ConstraintLayout: Siguiente
+        ViewGroup.LayoutParams btnSiguienteP = (ViewGroup.MarginLayoutParams) botonSiguiente.getLayoutParams();
+        btnSiguienteP.width = calWidth(buttonsX);
+        btnSiguienteP.height = calHeight(buttonsY);
+
+    }
+
+    /*
+     * ---Methods: AtuoFit------------------------------------------------------------------------*/
+    // Method: calHeight
+    public int calHeight (float value) {
+        return (int) (dpHeight * (value/desighHeiht));
+    }
+    // Method: calWidth
+    public int calWidth (float value) {
+        return (int) (dpWidth * (value/designWidth));
     }
 
 }
--- BookishBookshop/BookishBookshop/app/src/main/java/com/example/bookishbookshop/PreferenciasTwo.java
+++ BookishBookshop/BookishBookshop/app/src/main/java/com/example/bookishbookshop/PreferenciasTwo.java
@@ -4,18 +4,36 @@ import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class PreferenciasTwo extends AppCompatActivity {
 
+    /*
+     * ---Variables--------------------------------------------------------------------------------*/
+    ImageView logo;
+    ScrollView scrvP2;
     ConstraintLayout botonOmitir,botonSiguiente;
+
+    /*
+     * ---AutoFit Variables-----------------------------------------------------------------------*/
+    private int dpWidth, dpHeight, designWidth = 360,  desighHeiht = 780;
+    private float dDensity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_preferencias_dos);
 
+        /*
+         * ---Asignar Variables-------------------------------------------------------------------*/
+        logo = findViewById(R.id.iv_Logo_Preferences2);
+        scrvP2 = findViewById(R.id.scrollView_Preferences2);
         botonOmitir = findViewById(R.id.btnOmitir);
         botonSiguiente = findViewById(R.id.btnSiguiente);
 
@@ -34,5 +52,39 @@ public class PreferenciasTwo extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+
+        /*
+         * ---AutoFit-----------------------------------------------------------------------------*/
+        int buttonsX = 130, buttonsY = 60, edittextX = 300, edittextY = 70;
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        dpWidth = (displayMetrics.widthPixels);
+        dpHeight = (displayMetrics.heightPixels);
+        dDensity = (displayMetrics.scaledDensity);
+        // Imageview: Logo
+        ViewGroup.LayoutParams logoP = (ViewGroup.MarginLayoutParams) logo.getLayoutParams();
+        logoP.height = calHeight(120);
+        // ScrollView: scrollView_Preferences1
+        ViewGroup.LayoutParams scrvP1P = (ViewGroup.MarginLayoutParams) scrvP2.getLayoutParams();
+        scrvP1P.height = calHeight(608);
+        // ConstraintLayout: Cancelar
+        ViewGroup.LayoutParams btnOmitirP = (ViewGroup.MarginLayoutParams) botonOmitir.getLayoutParams();
+        btnOmitirP.width = calWidth(buttonsX);
+        btnOmitirP.height = calHeight(buttonsY);
+        // ConstraintLayout: Siguiente
+        ViewGroup.LayoutParams btnListoP = (ViewGroup.MarginLayoutParams) botonSiguiente.getLayoutParams();
+        btnListoP.width = calWidth(buttonsX);
+        btnListoP.height = calHeight(buttonsY);
+
+    }
+
+    /*
+     * ---Methods: AtuoFit------------------------------------------------------------------------*/
+    // Method: calHeight
+    public int calHeight (float value) {
+        return (int) (dpHeight * (value/desighHeiht));
+    }
+    // Method: calWidth
+    public int calWidth (float value) {
+        return (int) (dpWidth * (value/designWidth));
     }
 }
